d556c4fe29c05c4cae90db94b92eb0ff0b3e78d0
EbadShelby <ebadyasser123xd@gmail.com>
2025-02-19 09:10:31 +0800

    loop in ads carousel slide

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,17 @@ const dotsNav = document.querySelector(".ads-carousel__nav");
 const dots = Array.from(dotsNav.children);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
+
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + "px";
+};
+slides.forEach(setSlidePosition);
+
+// slides[0].style.left = slideWidth * 0 + "px";
+// slides[1].style.left = slideWidth * 1 + "px";
+// slides[2].style.left = slideWidth * 2 + "px";
+
+nextButton.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector("ads-carousel__current-slide");
+  const nextSlide = currentSlide.nextElementSibling;
+});
